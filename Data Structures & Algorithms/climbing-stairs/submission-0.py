eb55b756057893ec2c78a2e23f671278ff949a35
Windows 11 <169090607+Ankit-20285@users.noreply.github.com>
2026-07-09 02:58:40 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo  = {}
+        def dfs(n): 
+            if n == 0: 
+                return 1 
+            
+            if n == 1: 
+                return 1 
+
+            if n not in memo: 
+                memo[n] = dfs(n - 2) + dfs(n - 1)
+            
+            return memo[n]
+            
+        
+        return dfs(n)
